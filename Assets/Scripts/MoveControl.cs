21f27005beb1ca1fb5c59ee922a0dbd9e86913e6
Postovoi Alexander <postovoialex@gmail.com>
2024-04-03 20:34:48 +0300

    an attempt to implement a teleport
    -add tag border
    -add left and right border
    -add OnTriggerEnter2D method for teleport

--- Assets/Scripts/MoveControl.cs
+++ Assets/Scripts/MoveControl.cs
@@ -1,28 +1,29 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MoveControl : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb2;
+    [SerializeField] Collider2D doodlerCollider;
     [SerializeField] float speed = 5f;
+    [SerializeField] Doodler doodler;
     float direction = 0;
-
-    //private void Update()
-    //{
-    //    float moveHorizontal = Input.GetAxis("Horizontal");
-    //    rb2.velocity = new Vector2(moveHorizontal * speed, rb2.velocity.y);
-    //    Debug.Log(moveHorizontal);
-    //}
 
     private void Update()
     {
+
+        //    float moveHorizontal = Input.GetAxis("Horizontal");
+        //    rb2.velocity = new Vector2(moveHorizontal * speed, rb2.velocity.y);
+        //    Debug.Log(moveHorizontal);
+
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("A key pressed");
+            //    Debug.Log("A key pressed");
             direction = -1;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("D key pressed");
+            //   Debug.Log("D key pressed");
             direction = 1;
         }
         else
@@ -32,8 +33,19 @@ public class MoveControl : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("border"))
+        {
+            Debug.Log("Border");
+            doodler.transform.position = new Vector2(doodler.transform.position.x * -1, doodler.transform.position.y); 
+        }
+    }
+
+
+
     private void FixedUpdate()
     {
-        rb2.velocity = new Vector2 (direction * speed, rb2.velocity.y);
+        rb2.velocity = new Vector2(direction * speed, rb2.velocity.y);
     }
 }
